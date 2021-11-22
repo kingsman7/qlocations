9e3016061a6f3ead35e4353004a15f7bd96d2545
msolanogithub <msolano2505@gmail.com>
2021-11-22 12:41:41 -0500

    +Update: Neighbordhoods CRUD

--- _crud/neighborhoods.vue
+++ _crud/neighborhoods.vue
@@ -1,75 +1,153 @@
 <template></template>
 
 <script>
-  export default {
-    data() {
+export default {
+  data() {
+    return {
+      crudId: this.$uid()
+    }
+  },
+  computed: {
+    crudData() {
       return {
-        crudId: this.$uid()
-      }
-    },
-    computed: {
-      crudData() {
-        return {
-          crudId: this.crudId,
-          entityName: config("main.qlocations.entityNames.neighborhood"),
-          apiRoute: 'apiRoutes.qlocations.neighborhoods',
-          //permission: 'icommerce.manufacturers',
-          create: {
-            title: this.$tr('qlocations.layout.newNeighborhood'),
-            to: {name: 'qlocations.admin.neighborhoods.create'}
-          },
-          read: {
-            columns: [
-              {name: 'id', label: this.$tr('ui.form.id'), field: 'id', style: 'width: 50px'},
-              {name: 'name', label: this.$tr('ui.form.name'), field: 'name', align: 'left'},
-              {
-                name: 'created_at', label: this.$tr('ui.form.createdAt'), field: 'createdAt', align: 'left',
-                format: val => val ? this.$trd(val) : '-',
+        crudId: this.crudId,
+        entityName: config("main.qlocations.entityNames.neighborhood"),
+        apiRoute: 'apiRoutes.qlocations.neighborhoods',
+        permission: 'ilocations.neighborhoods',
+        create: {
+          title: this.$tr('qlocations.layout.newNeighborhood')
+        },
+        read: {
+          columns: [
+            {name: 'id', label: this.$tr('ui.form.id'), field: 'id', style: 'width: 50px'},
+            {name: 'name', label: this.$tr('ui.form.name'), field: 'name', align: 'left'},
+            {
+              name: 'city', label: this.$tr('ui.form.city'), field: 'city', align: 'left',
+              format: val => val ? val.name : '-',
+            },
+            {
+              name: 'province', label: this.$tr('qlocations.layout.form.province'), field: 'province', align: 'left',
+              format: val => val ? val.name : '-',
+            },
+            {
+              name: 'country', label: this.$tr('ui.label.country'), field: 'country', align: 'left',
+              format: val => val ? val.name : '-',
+            },
+            {
+              name: 'created_at', label: this.$tr('ui.form.createdAt'), field: 'createdAt', align: 'left',
+              format: val => val ? this.$trd(val) : '-',
+            },
+            {
+              name: 'updatedAt', label: this.$tr('ui.form.updatedAt'), field: 'updatedAt', align: 'left',
+              format: val => val ? this.$trd(val) : '-',
+            },
+            {name: 'actions', label: this.$tr('ui.form.actions'), align: 'left'},
+          ],
+          requestParams: {include: 'city,province,country'},
+          filters: {
+            countryId: {
+              value: null,
+              type: 'select',
+              props: {
+                label: this.$tr('qlocations.layout.form.country'),
+                clearable: true
               },
-              {name: 'actions', label: this.$tr('ui.form.actions'), align: 'left'},
-            ],
-            //requestParams: {include: 'parent'}
-            filters:{
-              country: {
-                value: null,
-                type: 'select',
-                props : {
-                  label: this.$tr('qlocations.layout.form.country'),
-                },
-                loadOptions: {
-                  apiRoute: 'apiRoutes.qlocations.countries',
-                  select: {label: 'name', id: 'id'},
-                }
+              loadOptions: {
+                apiRoute: 'apiRoutes.qlocations.countries',
+                select: {label: 'name', id: 'id'},
+              }
+            },
+            provinceId: {
+              value: null,
+              type: 'select',
+              props: {
+                label: this.$tr('qlocations.layout.form.province'),
+                clearable: true
               },
-              province: {
-                value: null,
-                type: 'select',
-                props : {
-                  label: this.$tr('qlocations.layout.form.province'),
-                },
-                loadOptions: {
-                  apiRoute: 'apiRoutes.qlocations.provinces',
-                  select: {label: 'name', id: 'id'},
-                }
+              loadOptions: {
+                apiRoute: 'apiRoutes.qlocations.provinces',
+                select: {label: 'name', id: 'id'},
+              }
+            },
+            cityId: {
+              value: null,
+              type: 'select',
+              props: {
+                label: this.$tr('ui.form.city'),
+                clearable: true
               },
+              loadOptions: {
+                apiRoute: 'apiRoutes.qlocations.cities',
+                select: {label: 'name', id: 'id'},
+              }
+            },
+          }
+        },
+        update: {
+          title: this.$tr('qlocations.layout.updateNeighborhood'),
+          requestParams: {include: 'city,province,country'}
+        },
+        delete: true,
+        formLeft: {
+          name: {
+            value: null,
+            type: 'input',
+            required: true,
+            isTranslatable: true,
+            props: {
+              label: `${this.$tr('ui.form.name')}*`
             }
           },
-          update: {
-            title: this.$tr('qlocations.layout.updateNeighborhood'),
-            to: 'qlocations.admin.neighborhoods.edit'
-            //requestParams: {include: 'parent'}
+          countryId: {
+            value: null,
+            type: 'select',
+            required: true,
+            props: {
+              label: `${this.$tr('ui.label.country')}*`
+            },
+            loadOptions: {
+              apiRoute: 'apiRoutes.qlocations.countries',
+              select: {label: 'name', id: 'id'},
+              //filterByQuery: true
+            }
           },
-          delete: true,
-          /*formLeft: {
+          provinceId: {
+            value: null,
+            type: 'select',
+            required: true,
+            props: {
+              label: `${this.$tr('qlocations.layout.form.province')}*`,
+              readonly: (this.crudInfo.countryId ? false : true)
+            },
+            loadOptions: {
+              apiRoute: 'apiRoutes.qlocations.provinces',
+              requestParams: {filter: {country_id: this.crudInfo.countryId}},
+              select: {label: 'name', id: 'id'},
+              //filterByQuery: true
+            }
           },
-          /*formRight: {
-          },*/
+          cityId: {
+            value: null,
+            type: 'select',
+            required: true,
+            props: {
+              label: `${this.$tr('ui.form.city')}*`,
+              readonly: (this.crudInfo.provinceId ? false : true)
+            },
+            loadOptions: {
+              apiRoute: 'apiRoutes.qlocations.cities',
+              requestParams: {filter: {province_id: this.crudInfo.provinceId}},
+              select: {label: 'name', id: 'id'},
+              //filterByQuery: true,
+            }
+          }
         }
-      },
-      //Crud info
-      crudInfo() {
-        return this.$store.state.qcrudComponent.component[this.crudId] || {}
       }
+    },
+    //Crud info
+    crudInfo() {
+      return this.$store.state.qcrudComponent.component[this.crudId] || {}
     }
   }
+}
 </script>
